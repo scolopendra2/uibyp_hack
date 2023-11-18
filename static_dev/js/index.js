70eda42bfb489c2a42c58c3e6d77c379646cc8b8
StanislavRND <stassinelnikov6@gmail.com>
2023-11-18 17:34:48 +0300

    Снова обновление Frontend

--- static_dev/js/index.js
+++ static_dev/js/index.js
@@ -1,18 +1,12 @@
 
-// const gifAnimation = document.querySelector('.main__gif-animation');
-// gifAnimation.addEventListener('mouseover', () => {
-//   gifAnimation.src = 'https://media1.giphy.com/media/i7C42rkeDTeeRz2KRq/giphy.gif?cid=ecf05e47hh6518c5spmwjfoecgmrn0ay9s7u4ovo5guxw9zk&ep=v1_stickers_search&rid=giphy.gif&ct=s';// Заменяем статическое изображение на gif
-// });
-// gifAnimation.addEventListener('mouseout', () => {
-//   gifAnimation.src = './img/image_2023-11-17_17-31-23.png'; // Заменяем gif на статическое изображение
-// });
 
 
 function sendData() {
 	var userInput = document.querySelector('.intro__input-input').value;
 	var csrfToken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+	var count = 0;
 
-	var data = { text: userInput };
+	var data = { text: userInput, count: count };
 
 	fetch('/bot/questions/', {
 		method: 'post',
