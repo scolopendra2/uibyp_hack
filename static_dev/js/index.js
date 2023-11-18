0821d181b38ea51ad5efec963f441dc3f2e2c715
Roma <romantkacenko82@gmail.com>
2023-11-18 18:08:40 +0300

    Изменение в js

--- static_dev/js/index.js
+++ static_dev/js/index.js
@@ -9,25 +9,31 @@
 
 
 function sendData() {
-	var userInput = document.querySelector('.intro__input-input').value;
-	var csrfToken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+    var userInput = document.querySelector('.intro__input-input').value;
+    var csrfToken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
 
-	var data = { text: userInput };
+    var data = { text: userInput };
 
-	fetch('/get_text/', {
-		method: 'post',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(data),
-	})
-		.then(response => response.json())
-		.then(data => {
-			console.log('Успешно отправлено:', data);
-		})
-		.catch(error => {
-			console.error('Ошибка:', error);
-		});
+    fetch('/bot/questions/', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrfToken,
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Успешно отправлено:', data);
+    })
+    .catch(error => {
+        console.error('Ошибка:', error);
+    });
 }
 
 
