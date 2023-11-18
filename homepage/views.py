88ee6cdfa90a42fde4df1e3b83717a8b6f9d4821
StanislavRND <stassinelnikov6@gmail.com>
2023-11-19 01:35:47 +0300

    Added

--- homepage/views.py
+++ homepage/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 
 
 def home(request):
-    template = 'homepage/home.html'
-    return render(request, template)
+		if request.method == 'POST':
+			
+		else:
+			template = 'homepage/home.html'
+			return render(request, template)
+
+
 
 
 def auth(request):
     template = 'homepage/auth.html'
     return render(request, template)
+
+	
--- static_dev/js/index.js
+++ static_dev/js/index.js
@@ -1,27 +1,38 @@
 function sendData() {
-    var userInput = document.querySelector('.intro__input-input').value;
-    var csrfToken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+	var userInput = document.querySelector('.intro__input-input').value;
+	var csrfToken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
 
-    var data = {text: userInput};
+  const message = document.querySelector('intro__message');
 
-    fetch('/bot/questions/', {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrfToken,
-        },
-        body: JSON.stringify(data),
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Успешно отправлено:', data);
-        })
-        .catch(error => {
-            console.error('Ошибка:', error);
-        })
-    }
+	message.textContent = userInput
+
+	var data = {text: userInput};
+
+	fetch('/', {
+			method: 'post',
+			headers: {
+					'Content-Type': 'application/json',
+					'X-CSRFToken': csrfToken,
+			},
+			body: JSON.stringify(data),
+	})
+			.then(response => {
+					if (!response.ok) {
+							throw new Error('Network response was not ok');
+					}
+					return response.json();
+			})
+			.then(data => {
+					console.log('Успешно отправлено:', data);
+			})
+			.catch(error => {
+					console.error('Ошибка:', error);
+			})
+	}
+
+	const userInput = document.querySelector('.intro__input-input');
+  const message = document.querySelector('intro__message');
+
+	userInput.addEventListener('input', () => {
+    message.innerText = userInput.value;
+  });
